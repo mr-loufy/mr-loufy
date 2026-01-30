04cc714f17cdaac1a6685ff0129e793ee75dc8db
mr-loufy <hamidrazajatt@gmail.com>
2026-01-30 11:46:44 +0500

    Create app/admin/login/page.tsx

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminLogin() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-2">Admin Login</h1>
+      <p>Authentication will be added.</p>
+    </>
+  )
+}
