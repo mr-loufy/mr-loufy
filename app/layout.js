a39630b3e4e8dd8375453295d7daa1279faa01b5
mr-loufy <hamidrazajatt@gmail.com>
2026-01-30 11:01:58 +0500

    Create layout.js
    Add layout

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Mr Loufy | Amazon KDP Publisher",
+  description: "Professional Amazon KDP publishing services"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
