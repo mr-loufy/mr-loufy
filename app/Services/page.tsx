889355940f7e1fd1697151d0d7d55cb35e266c71
mr-loufy <hamidrazajatt@gmail.com>
2026-01-30 11:43:53 +0500

    Create page.tsx

---
+++ app/Services/page.tsx
@@ -0,0 +1,15 @@
+export default function Services() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-6">Services</h1>
+
+      <ul className="space-y-4 max-w-2xl">
+        <li>Amazon KDP Publishing</li>
+        <li>Interior Formatting</li>
+        <li>Book Cover Design</li>
+        <li>A Plus Content</li>
+        <li>KDP Listing Optimization</li>
+      </ul>
+    </>
+  )
+}
