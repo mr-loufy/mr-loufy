000f782b89d12b13fb114491e1e8b72dc1b80b27
mr-loufy <hamidrazajatt@gmail.com>
2026-01-30 11:44:19 +0500

    Create lib/data.ts

---
+++ lib/data.ts
@@ -0,0 +1,10 @@
+export const portfolioItems = [
+  {
+    title: "Built Different",
+    description: "Cover design and formatting",
+  },
+  {
+    title: "The Boy Who Spoke to the Wind",
+    description: "Full publishing support",
+  },
+]
