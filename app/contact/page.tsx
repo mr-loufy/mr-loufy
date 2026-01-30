a94af3acf9d92d45af2f389011b74bd262f5bb1d
mr-loufy <hamidrazajatt@gmail.com>
2026-01-30 11:45:44 +0500

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-4">Start Your Project</h1>
+      <p>Email: your@email.com</p>
+      <p>WhatsApp: +92xxxxxxxxxx</p>
+    </>
+  )
+}
